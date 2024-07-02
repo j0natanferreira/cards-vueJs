eaa774a508c00e9798f49132f39419ce4b051701
Jonatan Ferreira <jonatan91668954@gmail.com>
2024-07-02 01:07:03 -0300

    Primeiro Commit

---
+++ src/components/CardsComponent.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="cards">
+    <div class="card-img">
+      <div class="img">
+        <img :src="avatar" alt="avatar" />
+      </div>
+    </div>
+    <div class="card-info">
+      <h3>{{ name }}</h3>
+      <p>
+        {{ description }}
+      </p>
+      <button>View More</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CardsComponent",
+  props: {
+    avatar: String,
+    name: String,
+    description: String,
+  },
+};
+</script>
+
+<style scoped>
+.cards {
+  width: 250px;
+  height: 300px;
+  border-radius: 20px;
+  margin: 10px;
+  background: #3d6ee5;
+  transition: 0.3s ease-in-out;
+}
+
+.cards:hover {
+  transform: translateY(-10px);
+  box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.1);
+}
+
+.card-img {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 50%;
+}
+
+.card-img > .img {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid #ffffff;
+  border-radius: 100%;
+  padding: 2px;
+}
+
+.card-img > .img > img {
+  width: 90px;
+  height: 90px;
+  border-radius: 100%;
+}
+
+.card-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  text-align: center;
+  gap: 4px;
+  padding: 10px;
+  width: 100%;
+  height: 50%;
+  background-color: #ffffff;
+  border-radius: 20px;
+}
+
+.card-info > p {
+  font-size: 16px;
+  color: grey;
+}
+
+.card-info > button {
+  background-color: #3d6ee5;
+  color: #ffffff;
+  border: none;
+  border-radius: 4px;
+  padding: 8px 14px;
+  cursor: pointer;
+  transition: 0.3s ease-in-out;
+}
+
+.card-info > button:hover {
+  background-color: #5680ea;
+}
+</style>
---
+++ src/components/SectionComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <section>
+    <CardsComponent
+      v-for="user in users"
+      :key="user.id"
+      :avatar="user.avatar"
+      :name="user.name"
+      :description="user.description"
+    />
+  </section>
+</template>
+
+<script>
+import CardsComponent from "./CardsComponent.vue";
+export default {
+  name: "SectionComponent",
+  components: {
+    CardsComponent,
+  },
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          avatar:
+            "https://cajamar.sp.gov.br/noticias/wp-content/uploads/sites/2/2021/08/site-vacinacao-26-anos.png",
+          name: "José Francisco",
+          description:
+            "Olá meu nome é José, sou uma pessoa seria e compromotido, vamos conversar mais.",
+        },
+        {
+          id: 2,
+          avatar: "https://tm.ibxk.com.br/2019/02/17/17124052466014.jpg",
+          name: "João Silva",
+          description:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias commodi eveniet.",
+        },
+        {
+          id: 3,
+          avatar:
+            "https://i.pinimg.com/236x/4c/20/2b/4c202b6376037a5fc660a6c7b6e55661.jpg",
+          name: "Maria de Souza",
+          description:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias commodi eveniet.",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  overflow: auto;
+}
+</style>
